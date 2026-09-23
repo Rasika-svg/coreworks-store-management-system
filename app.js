25c4824f4187c6a91cdd156b897c45bf697223e1
Rasika-svg <redussuriya@gmail.com>
2026-09-23 22:45:05 +0530

    Fix Firebase API key

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ import {
 ========================================================= */
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDXHA-L-TqWGi jVXD0k5nGw0YFrgzM5yA".replaceAll(" ", ""),
+    apiKey: "AIzaSyDXHA-L-IqWGi jVXDooQk5nGw0YFrgzM5yA".replaceAll(" ", ""),
     authDomain: "coreworks-store.firebaseapp.com",
     projectId: "coreworks-store",
     storageBucket: "coreworks-store.firebasestorage.app",
